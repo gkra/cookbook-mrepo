67a9bd450dfa52b4c567b30155a74c5c43aab5d3
Gregory Ruiz-Ade <gkra@unnerving.org>
2014-11-06 12:40:33 -0800

    rubocop fixes

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -8,12 +8,14 @@
 #
 
 # Create the main configuration file
-template "/etc/mrepo_conf" do
-    source "mrepo.conf.erb"
-    owner "root"
-    group "root"
-    mode "0644"
-    action :create
+template '/etc/mrepo_conf' do
+  source 'mrepo.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
+
 # Make sure the configuration directory exists
 directory '/etc/mrepo.conf.d' do
   owner 'root'
@@ -23,25 +25,25 @@ end
 
 # Create the repository configuration files
 node['mrepo']['repos'].each do |reponame, repodata|
-    template "/etc/mrepo.conf.d/#{reponame}.conf" do
-	source "mrepo_repo.conf.erb"
-	owner "root"
-	group "root"
-	mode "0644"
-	variables(
-	    :name => reponame,
-	    :repo => repodata
-	)
-	action :create
-    end
+  template "/etc/mrepo.conf.d/#{reponame}.conf" do
+    source 'mrepo_repo.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    variables(
+      :name => reponame,
+      :repo => repodata
+    )
+    action :create
+  end
 end
 
 # Create the apache configuration file
-template "/etc/httpd/conf.d/mrepo.conf" do
-    source "apache_mrepo.conf.erb"
-    owner "root"
-    group "root"
-    mode "0644"
-    action :create
+template '/etc/httpd/conf.d/mrepo.conf' do
+  source 'apache_mrepo.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
 end
 
